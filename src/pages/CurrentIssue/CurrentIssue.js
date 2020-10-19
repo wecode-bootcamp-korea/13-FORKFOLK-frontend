60a265112ed8bb9bb5f38d9239bd66954ae42872
Jean Baek <jeanbaek710@gmail.com>
2020-10-19 18:06:00 +0900

    Add: git 초기세팅

---
+++ src/pages/CurrentIssue/CurrentIssue.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CurrentIssue extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CurrentIssue
