0d56a0a4ee8d9d2ea48bb72f2990230100ce50cb
sunnysideup0w0v <723973972538@naver.com>
2020-10-20 10:24:17 +0900

    Add: footer Component 생성 - footer 컴포넌트 jsx 작성

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li className="depth01">
+              <ul className="depth02">
+                <li>Subscribe</li>
+                <li>Issue 37</li>
+                <li>Shop</li>
+              </ul>
+            </li>
+            <li>
+              <img src={"./images/forkfolk.jpg"} />
+            </li>
+            <li>
+              <ul>
+                <li>
+                  <i className="xi-search"></i>
+                </li>
+                <li>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div className="sideMenu"></div>
+      </header>
+    );
+  }
 }
 
-export default Header
+export default Header;
