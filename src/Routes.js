9c7ce178df4601cd3d0221b8882eaa160da2eaca
sunnysideup0w0v <723973972538@naver.com>
2020-10-20 13:42:24 +0900

    Add: header jsx 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 class Routes extends React.Component {
-    render() {
-      return (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={} />
-          </Switch>
-        </Router>
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Header} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "./forkfolk.png";
+import "./Header.scss";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                
+  constructor() {
+    super();
+    this.state = {
+      logoScale: 1,
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("wheel", (e) => {
+      console.log(e.wheelDelta);
+      this.setState({ logoScale: 2 });
+    });
+  }
+  render() {
+    return (
+      <>
+        <header className="Header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Subscribe</Link>
+              </li>
+              <li>
+                <Link to="/">Issue</Link>
+              </li>
+              <li>
+                <Link to="/">Shop</Link>
+              </li>
+            </ul>
+            <div
+              className="imgBox"
+              style={{ transform: `scale(${this.state.logoScale})` }}
+            >
+              <img src={logo} alt="logo" />
             </div>
-        )
-    }
+            <ul>
+              <li>
+                <button>
+                  <i className="xi-search"></i>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main></main>
+      </>
+    );
+  }
 }
 
-export default Header
+export default Header;
