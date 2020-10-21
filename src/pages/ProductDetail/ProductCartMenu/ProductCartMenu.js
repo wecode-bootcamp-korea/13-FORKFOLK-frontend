250255306aa7388f28080422bf944a41251f5563
yerin719 <59353169+yerin719@users.noreply.github.com>
2020-10-21 15:12:55 +0900

    Add : Product Description 여는 버튼 추가

--- src/pages/ProductDetail/ProductCartMenu/ProductCartMenu.js
+++ src/pages/ProductDetail/ProductCartMenu/ProductCartMenu.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import './ProductCartMenu.scss';
 
 class ProductCartMenu extends Component {
+    constructor() {
+        super();
+        this.state = {
+            countProduct: 0
+        }
+    }
+    ChangeCountProduct(e) {
+        const { countProduct } = this.state;
+        this.setState(e.target.name === "plus" ? { countProduct: countProduct++ } : { countProduct: countProduct-- });
+    }
 
-
-
-render() {
+    render() {
+        const { countProduct } = this.state;
     return (
         <div className="ProductCartMenu">
             <div>
-                <span>3</span>
-                <button>-</button>
-                <button>+</button>
+                <span>{countProduct}</span>
+                <button name="plus" onClick={this.ChangeCountProduct}>-</button>
+                <button name="minus" onClick={this.ChangeCountProduct}>+</button>
             </div>
         </div>
         );
--- src/pages/ProductDetail/ProductDescription/ProductDescription.js
+++ src/pages/ProductDetail/ProductDescription/ProductDescription.js
@@ -7,16 +7,21 @@ class ProductDescription extends Component {
         this.state = {
             productName: "Issue Thirty-Two",
             price: "18",
-            descriptions: [`Kinfolk was founded in Portland, operates out of Copenhagen, and has long had roots in Japan. In fact, since 2013, the magazine-and its ongoing exploration of quality of life – has been translated into Japanese at our sister office in the heart of Shibuya, Tokyo. On our most recent visit in March, local editors Mako Ayabe and Kota Engaku reminded us what life in the Japanese capital is like for those living there with a sense of intention and energy: one of the navigable neighbourhoods, courteous codes and cutting-edge culture. This is in stark contrast to the vision of Tokyo as a city that can feel impenetrable to the outsider.
-            All orders are shipped worldwide on our behalf by ALIUM, using DHL Express from Denmark. Shipping confirmation and tracking details will be sent by email. Please note that all prints will be shipped separately if ordering multiple items.`,
-            `50 x 70 cm
-            High quality Giclée* print on 265g fine art paper
-            Open edition
-            *Giclée is a fine art printing process combining long-lasting archival inks with high quality art paper achieving prints of high-quality and deep vibrant color.`,
-            `All orders are shipped worldwide on our behalf by ALIUM, using DHL Express from Denmark. Shipping confirmation and tracking details will be sent by email. Please note that all prints will be shipped separately if ordering multiple items.`
+            descriptions: [
+                {
+                    kategory : "DESCRIPTION",
+                    text :"Meet Socrate, the beloved cat of Luc Braquet—the French photographer responsible for some of Kinfolk’s most glamourous fashion editorials including The Big Swing and Summer at Sea. Originally commissioned for Kinfolk’s Summer 2019 issue, the series Le Chat Chic saw Braquet and stylist Tania Rat-Patron try something a little different: They transformed Socrate into a Parisian glamour puss—and one of the magazine’s most popular models of all time. For The Kinfolk Print Collection, Socrate waves a classic Hermès silk scarf with carefree abandon. Fur coat: Model’s own."
+                },
+                {
+                    kategory : "DETAILS",
+                    text :"Meet Socrate, the beloved cat of Luc Braquet—the French photographer responsible for some of Kinfolk’s most glamourous fashion editorials including The Big Swing and Summer at Sea. Originally commissioned for Kinfolk’s Summer 2019 issue, the series Le Chat Chic saw Braquet and stylist Tania Rat-Patron try something a little different: They transformed Socrate into a Parisian glamour puss—and one of the magazine’s most popular models of all time. For The Kinfolk Print Collection, Socrate waves a classic Hermès silk scarf with carefree abandon. Fur coat: Model’s own."
+                },
+                {
+                    kategory : "SHIPPING + HANDING",
+                    text :"Meet Socrate, the beloved cat of Luc Braquet—the French photographer responsible for some of Kinfolk’s most glamourous fashion editorials including The Big Swing and Summer at Sea. Originally commissioned for Kinfolk’s Summer 2019 issue, the series Le Chat Chic saw Braquet and stylist Tania Rat-Patron try something a little different: They transformed Socrate into a Parisian glamour puss—and one of the magazine’s most popular models of all time. For The Kinfolk Print Collection, Socrate waves a classic Hermès silk scarf with carefree abandon. Fur coat: Model’s own."
+                }
             ],
-            isDescriptionOpen: "",
-            buttonTest: "",
+            DescriptionBtnState: "null",
         }
     }
 
@@ -26,22 +31,25 @@ class ProductDescription extends Component {
     };
 
     hadleClick = async (e) => {
-        const { buttonTest } = this.state;
-        this.setState(buttonTest===e.target.name?{ buttonTest: "3" }:{ buttonTest: e.target.name });
+        const { DescriptionBtnState } = this.state;
+        console.log(e.target.name);
+        this.setState(DescriptionBtnState===e.target.name?{ DescriptionBtnState: "null" }:{ DescriptionBtnState: e.target.name });
     }
 
 render() {
-    const { productName, price, descriptions, isDescriptionOpen ,buttonTest} = this.state;
+    const { productName, price, descriptions, DescriptionBtnState } = this.state;
         return (
                 <div className="ProductDescription">
                     <h1>{productName}</h1>
                     <h2>${price}</h2>
-                <div className={isDescriptionOpen ? "openDecription" : "closeDecription"}>{descriptions}</div>
-                <button className="button" name="" onClick={this.openDescription} >+more</button>
-                
-                <button onClick={this.hadleClick} name="0" className={buttonTest==="0" ? "click" : "noClick"} >0</button>
-                <button onClick={this.hadleClick} name="1" className={buttonTest==="1" ? "click" : "noClick"} >1</button>
-                <button onClick={this.hadleClick} name="2" className={buttonTest==="2" ? "click" : "noClick"} >2</button>
+                {
+                    descriptions.map((el, idx) =>
+                        <>
+                        <button onClick={this.hadleClick} name={idx} ><span>{el.kategory}</span><span>+</span></button>
+                        <div name={idx} className={DescriptionBtnState === `${idx}` ? "openDecription" : "closeDecription"}>{el.text} </div>
+                        </>
+                    )
+                }
 
                 </div>
 
