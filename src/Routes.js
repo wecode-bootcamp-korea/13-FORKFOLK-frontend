ab4f86674785997e568e5e8b10b7006db2dd4e36
sunnysideup0w0v <723973972538@naver.com>
2020-10-21 20:11:34 +0900

    Remove : 불필요 border 제거

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/" component={} />
+          <Route exact path="/" component={Main} />
         </Switch>
       </Router>
     );
