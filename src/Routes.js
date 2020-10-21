b25212b682818862e11e4d342ec3563c209e16ce
sunnysideup0w0v <723973972538@naver.com>
2020-10-21 20:31:40 +0900

    Remove: 불필요 요소 제거

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={} />
         </Switch>
       </Router>
     );
