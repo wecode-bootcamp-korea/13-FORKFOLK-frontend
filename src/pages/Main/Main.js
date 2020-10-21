04fb488bdb9a1302874d806c89315d31e9a8b69b
sunnysideup0w0v <723973972538@naver.com>
2020-10-21 15:03:10 +0900

    Add : Main.js 에 헤더 핸들러 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,20 +5,55 @@ import DiveIn from "./components/DivIn";
 import "./Main.scss";
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      logoScale: 8,
+      wheelDeltaCount: 0,
+      logoMarginTop: 400,
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("wheel", (e) => {
+      const { logoScale, wheelDeltaCount, logoMarginTop } = this.state;
+      const { wheelDelta } = e;
+      if (window.pageYOffset > 1000) {
+        return;
+      }
+      this.setState({ wheelDeltaCount: wheelDelta / 120 }, () => {
+        if (logoScale + wheelDeltaCount < 1 || logoMarginTop < 0) {
+          this.setState({ logoScale: 1, logoMarginTop: 0 });
+        } else if (logoScale + wheelDeltaCount > 8 || logoMarginTop > 400) {
+          this.setState({ logoScale: 8, logoMarginTop: 400 });
+        } else {
+          this.setState({
+            logoScale: logoScale + wheelDeltaCount,
+            logoMarginTop: logoMarginTop + wheelDeltaCount * 100,
+          });
+        }
+      });
+    });
+  }
   render() {
     return (
-      <main className="Main">
-        <div className="mainIssue">
-          <span className="quickSand">ARTS &#38; CULTURE</span>
-          <br />
-          Navigate Nature
-          <br />
-          <span>How to orienteer outdoors</span>
-        </div>
-        <SelectedStories />
-        <ExtraordinaryInteriors />
-        <DiveIn />
-      </main>
+      <>
+        {/* <Header
+          logoScale={this.state.logoScale}
+          logoMarginTop={this.state.logoMarginTop}
+        /> */}
+        <main className="Main">
+          <div className="mainIssue">
+            <span className="quickSand">ARTS &#38; CULTURE</span>
+            <br />
+            Navigate Nature
+            <br />
+            <span>How to orienteer outdoors</span>
+          </div>
+          <SelectedStories />
+          <ExtraordinaryInteriors />
+          <DiveIn />
+        </main>
+      </>
     );
   }
 }
