52da677313b1c2f2f7b5024b6192b8508e24db23
sunnysideup0w0v <723973972538@naver.com>
2020-10-22 21:33:25 +0900

    Add : TextSticky Component 생성 - TextSticky 컴포넌트 생성 및 props로 내부 내용 설정 - position sticky를 이용한 내부 내용 설정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,6 +5,8 @@ import CurrentIssue from "./components/CurrentIssue";
 import MeetThePoets from "./components/MeetThePoets";
 import Popular from "./components/Popular";
 import ExpandedView from "./components/ExpandedView";
+import FashionIssue from "./components/FashionIssue";
+import TextSticky from "./components/TextSticky";
 
 class Main extends Component {
   constructor() {
@@ -19,22 +21,28 @@ class Main extends Component {
       diveIn: [],
     };
   }
-  componentDidMount() {
-    fetch("http://localhost:3000/Data/FlexItemsData.json")
-      .then((res) => res.json())
-      .then((result) =>
-        this.setState({
-          extraordinaryInteriors: result.flexItemsData.ExtraordinaryInteriors,
-          selectedStories: result.flexItemsData.SelectedStories,
-          diveIn: result.flexItemsData.DiveIn,
-        })
-      );
+  fetchData = async () => {
+    const fetch1 = await fetch("http://localhost:3000/Data/FlexItemsData.json");
+    const fetch2 = await fetch(
+      "http://localhost:3000/Data/ExpandedViewData.json"
+    );
+    const json1 = await fetch1.json();
+    const json2 = await fetch2.json();
+    const selectedStories = json1.flexItemsData.selectedStories;
+    const extraordinaryInteriors = json1.flexItemsData.extraordinaryInteriors;
+    const diveIn = json1.flexItemsData.diveIn;
+    const expandedViewContents = json2.expandedViewContents;
 
-    fetch("http://localhost:3000/Data/ExpandedViewData.json")
-      .then((res) => res.json())
-      .then((result) =>
-        this.setState({ expandedViewContents: result.expandedView })
-      );
+    this.setState({
+      selectedStories,
+      extraordinaryInteriors,
+      diveIn,
+      expandedViewContents,
+    });
+  };
+
+  componentDidMount() {
+    this.fetchData();
 
     window.addEventListener("wheel", (e) => {
       const { logoScale, wheelDeltaCount, logoMarginTop } = this.state;
@@ -73,22 +81,42 @@ class Main extends Component {
           logoMarginTop={logoMarginTop}
         /> */}
         <main className="Main">
-          <div className="mainIssue">
-            <span className="quickSand">ARTS &#38; CULTURE</span>
-            <br />
-            Navigate Nature
-            <br />
-            <span>How to orienteer outdoors</span>
-          </div>
-          {expandedViewContents && (
+          <TextSticky
+            subtitle="ARTS &#38; CULTURE"
+            title="Navigate to Nature"
+            description="How to orienteer outdoors"
+            backgroundSrc={
+              "https://images.unsplash.com/photo-1422564030440-1ecae6e21f67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2601&q=80"
+            }
+          />
+          {expandedViewContents.length && (
             <ExpandedView contents={expandedViewContents[0]} />
           )}
           <CurrentIssue />
           <FlexItemsList title="Selected Stories" contents={selectedStories} />
+          <TextSticky
+            title="Kinfolk Dosan"
+            description="A New Community in Seoul"
+            backgroundSrc={
+              "https://images.unsplash.com/photo-1581392766328-fb75f336149c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+            }
+          />
+          <FashionIssue />
+          <TextSticky
+            subtitle="DESIGN"
+            title="DOWNSIZING"
+            description="Unable to travel during lockdown, architects Salem Charabi &#38; Rasmus Stroyberg decided to recreate a favorite building."
+            backgroundSrc={
+              "https://images.unsplash.com/photo-1508330570239-ce7cabceee22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1756&q=80"
+            }
+          />
           <FlexItemsList
             title="Extranordinary Interiors"
             contents={extraordinaryInteriors}
           />
+          {expandedViewContents.length && (
+            <ExpandedView contents={expandedViewContents[1]} />
+          )}
           <FlexItemsList title="Dive In" contents={diveIn} />
           <MeetThePoets />
           <Popular />
--- src/pages/Main/components/ExpandedView.js
+++ src/pages/Main/components/ExpandedView.js
@@ -11,15 +11,14 @@ class ExpandedView extends Component {
       imgSrc,
     } = this.props.contents;
     return (
-      //   <div className="ExpandedView">
-      //     <div>
-      //       <h3>{field}</h3>
-      //       <span>{title}</span>
-      //       <p className={artsnCulture && "artsnCulture"}>{description}</p>
-      //     </div>
-      //     <img alt="representative" src={imgSrc} />
-      //   </div>
-      <div>Tello</div>
+      <div className="ExpandedView">
+        <div>
+          <h3>{field}</h3>
+          <span>{title}</span>
+          <p className={!artsnCulture ? "artsnCulture" : ""}>{description}</p>
+        </div>
+        <img alt="representative" src={imgSrc} />
+      </div>
     );
   }
 }
