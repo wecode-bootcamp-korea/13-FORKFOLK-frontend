54362a4bc57e741cffb6ed96ba27f4ce76e1c736
sunnysideup0w0v <723973972538@naver.com>
2020-10-23 18:18:40 +0900

    Refactor : 불필요 route 삭제

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={Header} />
-        </Switch>
+        <Switch></Switch>
         <Route path="/" component={Footer} />
       </Router>
     );
