1394deab89f8384bc1ff6cdbbe5d76f5485918a5
yerin719 <59353169+yerin719@users.noreply.github.com>
2020-10-27 16:22:39 +0900

    Add : login input 로직수정

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ProductDetail extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default ProductDetail
+export default ProductDetail;
