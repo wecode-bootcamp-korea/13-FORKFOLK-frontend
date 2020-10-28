2f074ce1993c38cf6a149929861e4f003328c0a4
sunnysideup0w0v <723973972538@naver.com>
2020-10-28 20:53:19 +0900

    Add : CHECKOUT_API 추가

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const APIROOT = "http://localhost:3000";
 
 export const PRODUCT_DETAIL_API = "http://localhost:3000";
-export const CHECKOUT_API = "http://localhost:3000/Data/";
+export const CHECKOUT_API = "http://localhost:3000/Data/cartList.json";
 export const LOCAL_API_HJ = "http://localhost:3000";
 export let TitleContents = [
   {
