1ea0897f73c4aa0b0909f43040bd6e1aa6b426d8
sunnysideup0w0v <723973972538@naver.com>
2020-10-28 20:04:29 +0900

    Add : CheckoutFormInfo.js 추가 - MockData

---
+++ src/pages/Checkout/CheckoutFormInfo.js
@@ -0,0 +1,22 @@
+const FORM_INFO = [
+  {
+    id: 1,
+    labelTitle: "Name",
+    inputName: "name",
+    placeHolder: "Please input your name.",
+  },
+  {
+    id: 2,
+    labelTitle: "Address",
+    inputName: "address",
+    placeHolder: "Please input your address.",
+  },
+  {
+    id: 3,
+    labelTitle: "Phone Number",
+    inputName: "phone_number",
+    placeHolder: "Please input your phone number.",
+  },
+];
+
+export default FORM_INFO;
